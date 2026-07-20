72e913bdab6d68cdc6069a6544dcca55c4c4362a
byna1 <barbaraynaye@gmail.com>
2026-07-20 15:04:48 +0200

    awayTeamId added into matches_large table

--- src/gold/R_matches_large.sql
+++ src/gold/R_matches_large.sql
@@ -6,6 +6,7 @@ SELECT
     SUBSTR(match_date,1,16) as match_date,
     country_code AS league_country_code,
     homeTeam_id,
+    awayTeam_id,
     current_home_team_score AS home_current_sets_won,
     current_away_team_score AS away_current_sets_won,
     firstSet_home_team_score,
