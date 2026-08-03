eedb10ec2846f97f1d3f370e2199ebed5e59369e
byna1 <barbaraynaye@gmail.com>
2026-08-03 09:31:12 +0200

    R_dim_teams

--- src/gold/R_dim_teams.sql
+++ src/gold/R_dim_teams.sql
@@ -1,26 +1,25 @@
-WITH 
-
-tb_teams
-
-AS 
-
-
-(
+WITH tb_teams AS (
     SELECT
         team_id,
-        team_logo,
-        TRIM(REPLACE(team_name,'Women','')) AS team_name,
+        TRIM(REPLACE(team_name, 'Women', '')) AS team_name,
         CASE WHEN team_name LIKE '%Women%' THEN 'Women' ELSE 'Men' END AS team_naipe
     FROM teams
+),
+
+tb_team_country AS (
+    SELECT homeTeam_id AS team_id, country_code AS country_id FROM matches
+    UNION
+    SELECT awayTeam_id AS team_id, country_code AS country_id FROM matches
 )
 
-
 SELECT
-    team_id,
-    COALESCE(
-        NULLIF(team_logo,''),
-        MAX(NULLIF(team_logo,'')) OVER (PARTITION BY team_name)
-    ) AS team_logo,
-    team_name,
-    team_naipe
-FROM tb_teams
+    t1.team_id AS Team_id,
+    t2.country_id,
+    t1.team_name,
+    t1.team_naipe,
+    CASE 
+        WHEN t2.country_id = 'World' THEN 'International' ELSE 'National'
+    END AS team_type
+FROM tb_teams AS t1
+LEFT JOIN tb_team_country AS t2
+    ON t1.team_id = t2.team_id
